22d5acdb98297fc20bf2c11871d8d91323d444af
phantom1003 <phantom@zju.edu.cn>
2023-02-13 22:14:14 +0800

    Use system default encoding

--- ev1-dec.py
+++ ev1-dec.py
@@ -1,14 +1,17 @@
-import os, tkinter, windnd
+import os, tkinter, windnd, chardet, locale
 
 def dnd_file(files):
     for file in files:
-        file = file.decode()
-        
+        try:
+            file = file.decode(sys_enc)
+        except UnicodeDecodeError:
+            file = file.decode('utf-8')
+
         if (file.split(".")[-1] != "ev1"):
-            print("Ignore " + file)
+            print(f"Ignore {file}")
             continue
 
-        print("Convert " + file)
+        print(f"Convert {file}")
         with open(file, 'rb+') as f:
             raw = f.read(100)
             data = bytearray(raw)
@@ -23,10 +26,11 @@ def dnd_file(files):
 
             os.rename(file, file + '.flv')
 
+sys_enc = locale.getpreferredencoding()
 
 root = tkinter.Tk()
 root.geometry('400x300')
-root.title('EV1 decode')
+root.title('DV1: EV1 Decoder')
 windnd.hook_dropfiles(root, func=dnd_file)
 label = tkinter.Label(root, text ='Drop *.ev1 on me :)')
 label.place(relx = 0.5, rely = 0.5, anchor = 'center')
